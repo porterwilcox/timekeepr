142f924ecab7de65226f72b4125ebd1686a8c853
Porter Wilcox <wilcox.porter@gmail.com>
2018-11-02 19:10:51 -0600

    time controller to handle api requests

--- Controllers/TimeController.cs
+++ Controllers/TimeController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using time.Models;
@@ -15,22 +16,44 @@ namespace time.Controllers
 
         [HttpPost]
         [Route("register")]
-        public Business Post([FromBody] BusinessRegistration creds)
+        public Business CreateBusiness([FromBody] BusinessRegistration creds)
         {
             if (!ModelState.IsValid) throw new Exception("Invalid information");
             return _repo.Register(creds);
         }
 
         [HttpGet("manager/{managerId}")]
-        public Business Get([FromRoute] string managerId)
+        public Business GetBusinessForManager([FromRoute] string managerId)
         {
             return _repo.GetBusinessByManager(managerId);
         }
 
         [HttpGet("employee/{employeeId}")]
-        public Business Get([FromRoute] string employeeId)
+        public Business GetBusinessForEmployee([FromRoute] string employeeId)
         {
             return _repo.GetBusinessByEmployee(employeeId);
+        }
+
+        [HttpPost]
+        [Route("enroll")]
+        public Business EnrollInBusiness([FromBody] EmployeeRegisterToBusiness creds)
+        {
+            if (!ModelState.IsValid) throw new Exception("Invalid information.");
+            return _repo.AddEmployeeToBusiness(creds);
+        }
+
+        [HttpGet("employees/{businessId}")]
+        public IEnumerable<User> GetEmployeesForManager(string businessId)
+        {
+            return _repo.GetEmployeesByBusinessId(businessId);
+        }
+
+        [HttpPost]
+        [Route("clockin")]
+        public UserTime CreateClockIn([FromBody] UserTime time)
+        {
+            if (!ModelState.IsValid) throw new Exception("Invalid information");
+            return _repo.CreateClockIn(time);
         }
 
 
