4eb0328940d7fdf93b62501b37d926c12e01f50b
Porter Wilcox <wilcox.porter@gmail.com>
2018-11-03 13:01:25 -0600

    bootstrap vue decided against scss

--- wwwsrc/src/views/Home.vue
+++ wwwsrc/src/views/Home.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="home">
+    <b-alert show variant="info">
+      <b-link to="/login" class="alert-link">click here to go to login</b-link>
+    </b-alert>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home'
-}
+  name: "home"
+};
 </script>
--- wwwsrc/src/views/Login.vue
+++ wwwsrc/src/views/Login.vue
@@ -1,8 +1,77 @@
 <template>
-    
+    <div class="login container-fluid">
+        <div class="row h-25">
+            <div class="col d-flex justify-content-center align-items-center">
+                <h1>TimeKeepr</h1>
+            </div>
+        </div>
+        <div class="row">
+            <b-card v-if="loginForm" class="col-10 offset-1 col-md-8 offset-md-2 bg-primary text-white">
+                <b-navbar class="d-flex justify-content-between bg-primary">
+                    <h1>Login</h1>
+                    <button @click="loginForm = !loginForm" class="btn btn-secondary">or signup</button>
+                </b-navbar>
+                <b-form @submit.prevent="submit()" @reset.prevent="clear()" class="px-3">
+                    <b-form-group label="email">
+                        <b-form-input v-model="login.email" type="email" required></b-form-input>
+                    </b-form-group>
+                    <b-form-group label="password">
+                        <b-form-input v-model="login.password" type="password" required></b-form-input>
+                    </b-form-group>
+                    <button type="submit" class="btn btn-secondary">submit</button>
+                </b-form>
+            </b-card>
+            <b-card v-else class="col-10 offset-1 col-md-8 offset-md-2 bg-secondary">
+                <b-navbar class="d-flex justify-content-between bg-secondary">
+                    <h1>Signup</h1>
+                    <button @click="loginForm = !loginForm" class="btn btn-primary">or login</button>
+                </b-navbar>
+                <b-form @submit.prevent="submit()" @reset.prevent="clear()" class="px-3">
+                    <b-form-group label="first name">
+                        <b-form-input v-model="register.firstName" required></b-form-input>
+                    </b-form-group>
+                    <b-form-group label="last name">
+                        <b-form-input v-model="register.lastName" required></b-form-input>
+                    </b-form-group>
+                    <b-form-group label="email">
+                        <b-form-input v-model="register.email" required type="email"></b-form-input>
+                    </b-form-group>
+                    <b-form-group label="password">
+                        <b-form-input v-model="register.password" required type="password"></b-form-input>
+                    </b-form-group>
+                    <button type="submit" class="btn btn-primary">submit</button>                    
+                </b-form>
+            </b-card>
+        </div>
+    </div>
 </template>
 <script>
 export default {
-    name: "login"
-}
+  name: "login",
+  data() {
+      return {
+          loginForm: true,
+          login: {
+              email: "",
+              password: ""
+          },
+          register: {
+              email: "",
+              password: "",
+              firstName: "",
+              lastName: ""     
+          }
+      }
+  },
+  methods: {
+      submit() {
+          console.log(this.login.email)
+      }
+  }
+};
 </script>
+<style>
+.login {
+    height: 100vh;
+}
+</style>
